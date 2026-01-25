4afb8c4b009f5af4530376e3001c7d51fce7e585
mheshammohammed <mhesham.mohammed@mail.utoronto.ca>
2026-01-25 18:01:19 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.54%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-
         if (x==0) {return 0;}
-        int result=1;
-        while (result < (x/result)) {
-            result++;
+        int left=1;
+        int right=x;
+        int ans=0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        if (result > (x/result)) {
-            result--;
-        }
-        return result;
+        return ans;
     }
 };
